2776e04ebade9c6da6fca0531d9d6b728808215d
bagus ryzen <bagusprasojo@tigaserangkai.co.id>
2026-01-10 06:40:08 +0700

    update

--- generator/generate.py
+++ generator/generate.py
@@ -1,8 +1,10 @@
 import json
 import os
+import re
+
 
 BASE_DIR = os.path.dirname(__file__)
-OUTPUT_DIR = os.path.join(BASE_DIR, "..", "docs\p")
+OUTPUT_DIR = os.path.join(BASE_DIR, "..", "docs", "p")
 OUTPUT_DIR_HOME = os.path.join(BASE_DIR, "..", "docs")
 
 with open(os.path.join(BASE_DIR, "template.html"), encoding="utf-8") as f:
@@ -15,14 +17,20 @@ os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 link = "<ul>"
 for p in products:
+    title = p["title"]
+    title = title.title()
+
     html = template
-    html = html.replace("{{TITLE}}", p["title"])
+    html = html.replace("{{TITLE}}", title)
     html = html.replace("{{DESCRIPTION}}", p["description"])
     html = html.replace("{{IMAGE}}", p["image"])
     html = html.replace("{{AFFILIATE_URL}}", p["affiliate_url"])
 
-    slug = p["title"]
-    slug = slug.replace(" ", "-")
+    
+    slug = title.lower()
+    slug = re.sub(r'[^a-z0-9\s-]', '', slug)
+    slug = re.sub(r'\s+', '-', slug).strip('-')
+
 
     output_path = os.path.join(OUTPUT_DIR, f"{slug}.html")
     # print(output_path)
@@ -32,20 +40,20 @@ for p in products:
 
     print(f"✔ Generated: {slug}.html")
     description = ""
-    if (p["status1"]):
-        description = f"{description} *{p['status1']}*<br>"
+    if p.get("status1"):
+        description += f"*{p['status1']}*<br>"
 
-    if (p["status2"]):
-        description = f"{description} *{p['status2']}*<br>"
+    if p.get("status2"):
+        description += f"*{p['status2']}*<br>"
 
-    if (p["review"]):
-        description = f"{description} *Review : {p['review']}*<br>"
+    if p.get("review"):
+        description += f"*Review : {p['review']}*<br>"
 
     description = f"{description} <p>{p['description']}</p>"    
 
-    print(description)
+    # print(description)
 
-    link = f"{link} <li>{description}<a href='/produk-pilihan/p/{slug}.html'>{slug}.html</a></li>"
+    link = f"{link} <li>{description}<a href='/produk-pilihan/p/{slug}.html'>https://bagusprasojo.github.io/produk-pilihan/p/{slug}.html</a><hr></li>"
 
 link = f"{link} </ul>"
 with open(os.path.join(BASE_DIR, "template_home.html"), encoding="utf-8") as f:
